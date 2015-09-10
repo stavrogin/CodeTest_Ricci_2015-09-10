48c9455eb58f1c2dcc5dc980abe7c6a19db8ca04
Flavio Ricci <flavio.ricci82@gmail.com>
2015-09-10 19:02:10 +0200

    Implemented logic to generate the game description

--- codetest/src/main/java/it/codetest/helper/GameHelper.java
+++ codetest/src/main/java/it/codetest/helper/GameHelper.java
@@ -46,13 +46,29 @@ public class GameHelper {
 		if (player1Score > 3 || player2Score > 3) {
 			//if so, then check if he scored at least two points more than the opponent
 			int difference = Math.abs(player1Score - player2Score);
-			if (difference >= 2) {
+			// we are at advantages status: check if it is deuce or one player is in advantage, or if we have a winner			
+			if (difference == 0) {
+				//deuce
+				game.setStatusDescription(Constants.DEUCE);
+			} else if (difference ==1) {
+				//one player is in advantage
+				if (player1Score > player2Score) {
+					game.setStatusDescription(Constants.ADVANTAGE + Constants.SPACE + game.getPlayer1().getName());
+				} else {
+					game.setStatusDescription(Constants.ADVANTAGE + Constants.SPACE + game.getPlayer2().getName());
+				}
+			} else {
+				//there are two points of difference: we have a winner
 				if (player1Score > player2Score) {
 					game.setWinner(game.getPlayer1());
 				} else {
 					game.setWinner(game.getPlayer2());
 				}
+				game.setStatusDescription(Constants.GAME + Constants.SPACE + game.getWinner().getName());			
 			}
+		} else {
+			//case in which the score is lower or equals than 3 for both players
+			game.setStatusDescription(Constants.POINTS_MAP.get(game.getPlayer1().getScore()) + Constants.SPACE + Constants.POINTS_MAP.get(game.getPlayer2().getScore()));
 		}
 	}
 	
